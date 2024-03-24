fe252b9fef017a12f643ad3108be342025f24e39
ejl30380 <ejl30380@uga.edu>
2024-03-24 18:36:01 -0400

    Initial Commit

---
+++ app/src/main/java/edu/uga/cs/countryquiz/Country.java
@@ -0,0 +1,43 @@
+package edu.uga.cs.countryquiz;
+
+public class Country {
+    private long id;
+    private String country;
+    private String continent;
+
+    public Country(){
+        this.id = -1;
+        this.country = null;
+        this.continent = null;
+    }
+
+    public Country(String country, String continent){
+        this.id = -1;
+        this.country = country;
+        this.continent = continent;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+}
