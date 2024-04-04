2bb2b82d71469c9faa019af70acaf1d1b31e570d
ejl30380 <ejl30380@uga.edu>
2024-04-04 11:49:48 -0400

    Commit

---
+++ app/src/main/java/edu/uga/cs/countryquiz/CountryDBHelper.java
@@ -0,0 +1,69 @@
+package edu.uga.cs.countryquiz;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class CountryDBHelper extends SQLiteOpenHelper{
+
+    private static final String DEBUG_TAG = "CountryDBHelper";
+
+    private static final String DB_NAME = "countries.db";
+    private static final int DB_VERSION = 1;
+
+    // Define all names (strings) for table and column names.
+    // This will be useful if we want to change these names later.
+    public static final String TABLE_COUNTRIES = "jobleads";
+    public static final String COUNTRIES_COLUMN_ID = "_id";
+    public static final String COUNTRIES_COLUMN_COUNTRY = "country";
+    public static final String COUNTRIES_COLUMN_CONTINENT = "continent";
+
+    // This is a reference to the only instance for the helper.
+    private static CountryDBHelper helperInstance;
+
+    // A Create table SQL statement to create a table for job leads.
+    // Note that _id is an auto increment primary key, i.e. the database will
+    // automatically generate unique id values as keys.
+    private static final String CREATE_COUNTRIES =
+            "create table " + TABLE_COUNTRIES + " ("
+                    + COUNTRIES_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + COUNTRIES_COLUMN_COUNTRY + " TEXT, "
+                    + COUNTRIES_COLUMN_CONTINENT + " TEXT, "
+                    + ")";
+
+    // Note that the constructor is private!
+    // So, it can be called only from
+    // this class, in the getInstance method.
+    private CountryDBHelper( Context context ) {
+        super( context, DB_NAME, null, DB_VERSION );
+    }
+
+    // Access method to the single instance of the class.
+    // It is synchronized, so that only one thread can executes this method, at a time.
+    public static synchronized CountryDBHelper getInstance( Context context ) {
+        // check if the instance already exists and if not, create the instance
+        if( helperInstance == null ) {
+            helperInstance = new CountryDBHelper( context.getApplicationContext() );
+        }
+        return helperInstance;
+    }
+
+    // We must override onCreate method, which will be used to create the database if
+    // it does not exist yet.
+    @Override
+    public void onCreate( SQLiteDatabase db ) {
+        db.execSQL( CREATE_COUNTRIES );
+        Log.d( DEBUG_TAG, "Table " + TABLE_COUNTRIES + " created" );
+    }
+
+    // We should override onUpgrade method, which will be used to upgrade the database if
+    // its version (DB_VERSION) has changed.  This will be done automatically by Android
+    // if the version will be bumped up, as we modify the database schema.
+    @Override
+    public void onUpgrade( SQLiteDatabase db, int oldVersion, int newVersion ) {
+        db.execSQL( "drop table if exists " + TABLE_COUNTRIES );
+        onCreate( db );
+        Log.d( DEBUG_TAG, "Table " + TABLE_COUNTRIES + " upgraded" );
+    }
+}
